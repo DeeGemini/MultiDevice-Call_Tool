ad18b65dd9b290635b9624d2f7fcc8451df4e213
DeeGemini <zodiactheegemini@gmail.com>
2024-10-11 17:24:09 +0200

    added a remove device function

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,6 +3,16 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const Device = require('../models/Device');
 
+// Fetch all users
+const findAll= async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
 // Register user
 const register = async (req, res) => {
     const { firstname, lastname, email, password, phonenumber } = req.body;
@@ -97,6 +107,44 @@ const addDevice = async (req, res) => {
     user.devices.push(device._id);
     await user.save();
     res.status(201).json({ message: 'Device added successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+// Remove device from user's account
+const removeDevice = async (req, res) => {
+  try {
+    if (!req.user) {
+      return res.status(400).json({ message: 'User ID is missing' });
+    }
+
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    const { deviceId } = req.body;
+    if (!deviceId) {
+      return res.status(400).json({ message: 'Device ID is missing' });
+    }
+
+    // Find the device to remove
+    const device = await Device.findOne({ deviceId, user: user._id });
+    if (!device) {
+      return res.status(400).json({ message: 'Device not found in user\'s account' });
+    }
+
+    // Remove device from the user's device list
+    user.devices = user.devices.filter(deviceId => deviceId.toString() !== deviceId.toString());
+
+    // Remove the device from the database
+    await Device.findByIdAndDelete(device._id);
+
+    await user.save();
+    res.status(200).json({ message: 'Device removed successfully' });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Server error' });
