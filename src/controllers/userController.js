ba589c0ebb5eee47122ecc2c0e74111af6204eaf
DeeGemini <zodiactheegemini@gmail.com>
2024-10-11 18:12:40 +0200

    Added link and unlink device functions

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -151,6 +151,43 @@ const removeDevice = async (req, res) => {
   }
 };
 
+// Linking a device to a user's account
+const linkDevice = async (req, res) => {
+  const { userId, deviceId } = req.body;
+  const user = await User.findById(userId);
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+  // Check if the device is already linked to the user
+  const deviceExists = user.devices.some(device => device.deviceId === deviceId);
+  if (deviceExists) {
+    return res.status(400).json({ message: 'Device already linked to user' });
+  }
+  // Link the new device to the user
+  const newDevice = new Device({ deviceId, deviceName, user: user._id });
+  await newDevice.save();
+
+  res.status(201).json({ message: 'Device linked successfully', device: newDevice });
+};
+
+// Unlinking a device from a user's account
+const unlinkDevice = async (req, res) => {
+  const { userId, deviceId } = req.body;
+  const user = await User.findById(userId);
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+  // Find the device to unlink
+  const device = user.devices.find(device => device.deviceId === deviceId);
+  if (!device) {
+    return res.status(400).json({ message: 'Device not found in user\'s account' });
+  }
+  // Unlink the device from the user
+  user.devices = user.devices.filter(device => device.deviceId !== deviceId);
+  await user.save();
+  res.status(200).json({ message: 'Device unlinked successfully' });
+};
+
 
 module.exports = {
     register,
